5d71b2393026d5bdcec0ffebef31566acbfdff15
Rahul Kumar <rahulkumarnetid20@gmail.com>
2025-08-19 18:20:32 +0530

    Configuration Management in accounts, loans and cards microservices

---
+++ section6/v1-springboot/accounts/src/main/java/com/codeblocks/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.codeblocks.accounts.mapper;
+
+import com.codeblocks.accounts.dto.CustomerDto;
+import com.codeblocks.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
