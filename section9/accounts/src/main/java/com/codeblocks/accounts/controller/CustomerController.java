11f0c20e1cdb90768ebbd89936f570eb6762d11e
Rahul Kumar <rahulkumarnetid20@gmail.com>
2025-09-06 01:31:36 +0530

    Implemented cross cutting concerns like tracing using correlationId

--- section9/accounts/src/main/java/com/codeblocks/accounts/controller/CustomerController.java
+++ section9/accounts/src/main/java/com/codeblocks/accounts/controller/CustomerController.java
@@ -10,14 +10,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -33,6 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api" , produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
 public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     private final ICustomerService iCustomerService;
 
@@ -59,11 +60,12 @@ public class CustomerController {
     }
     )
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("smartBank-correlation-id")String correlationId, @RequestParam
                                                                    @Pattern(regexp = "(^$|[0-9]{10})" , message= "MobileNumber must be 10 digits")
                                                                    String mobileNumber)
     {
-        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber);
+        logger.debug("smartBank-correlation-id found :{}",correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(customerDetailsDto);
