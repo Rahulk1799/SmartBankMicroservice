2f66ab35a9aa3a4d4d2c7db196ed5fbf0e6f6db5
Rahul Kumar <rahulkumarnetid20@gmail.com>
2025-09-08 16:36:00 +0530

    Implementing the Circuit Breaker Pattern using the Resilience4j for fault tolerance

---
+++ section10/accounts/src/main/java/com/codeblocks/accounts/service/ICustomerService.java
@@ -0,0 +1,13 @@
+package com.codeblocks.accounts.service;
+
+import com.codeblocks.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return - Customer Details based on mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
+}
---
+++ section10/gatewayserver/src/main/java/com/codeblocks/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,41 @@
+package com.codeblocks.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator smartbankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/smartbank/accounts/**")
+						.filters( f -> f.rewritePath("/smartbank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/smartbank/loans/**")
+						.filters( f -> f.rewritePath("/smartbank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/smartbank/cards/**")
+						.filters( f -> f.rewritePath("/smartbank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
+
+	}
+
+}
